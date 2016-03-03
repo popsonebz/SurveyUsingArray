8fc840ce917ad8140cb354b36a18c0f61345c869
popoola <popoolaebenezer@gmail.com>
2016-03-03 07:40:19 +0200

    Added enhanced for loop to calculate total

--- src/Survey.java
+++ src/Survey.java
@@ -1,10 +1,16 @@
 // Created by popoola
 public class Survey {
     public static void main(String[] args){
-        int[] responses = {1,2,5,4,3,5,2,1,3,3,1,4,3,3,3,2,3,3,2,1};
+        int[] responses = {1,2,5,4,3,5,2,1,3,3,1,4,3,3,3,2,3,3,2,14};
         int[] frequency = new int[6];
         for (int i=0;i<responses.length;i++){
-            ++frequency[responses[i]];
+            try {
+                ++frequency[responses[i]];
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+                System.out.printf("    responses[%d ]= %d%n%n", i,responses[i]);
+            }
+
 
         }
         System.out.printf("%8s   %8s\n","Response","frequency");
@@ -12,5 +18,11 @@ public class Survey {
 
             System.out.printf("%7d%7d\n",i,frequency[i]);
         }
+        int total = 0;
+        for (int resp:
+             frequency) {
+            total +=resp;
+        }
+        System.out.printf("The total frequency is %d\n",total);
     }
 }
